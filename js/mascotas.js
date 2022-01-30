61b7ae0e797dc6c88a67e63adec3f3e3e660400b
Aviandro <aviandro@Lenoxs-MacBook-Pro.local>
2022-01-29 19:41:45 -0500

    funcion editar una mascota

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,8 +1,8 @@
 const listaMascotas = document.getElementById("lista-mascotas");
 const especie = document.getElementById("especie");
 const nombre = document.getElementById("nombre");
-const raza = document.getElementById("raza");
 const edad = document.getElementById("edad");
+const raza = document.getElementById("raza");
 const dueno = document.getElementById("dueno");
 const indice = document.getElementById("indice");
 const form = document.getElementById("form");
@@ -11,8 +11,8 @@ let mascotas = [
     {
         especie: "Gato",
         nombre: "Mittens",
-        raza: "Criollo",
         edad: "6 años",
+        raza: "Criollo",
         dueno: "Alejandro González"
     }
 ];
@@ -22,18 +22,18 @@ function listarMascotas() {
         <th scope="row">${index}</th>
         <td>${mascota.especie}</td>
         <td>${mascota.nombre}</td>
-        <td>${mascota.raza}</td>
         <td>${mascota.edad}</td>
+        <td>${mascota.raza}</td>
         <td>${mascota.dueno}</td>
         <td>
             <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-            <button type="button" class="btn btn-info editar" data-indice=${index}><i class="fas fa-edit"></i></button>
+            <button type="button" class="btn btn-info editar")"><i class="fas fa-edit"></i></button>
             <button type="button" class="btn btn-danger"><i class="fas fa-trash"></i></button>
             </div>
         </td>
     </tr>`).join("");
     listaMascotas.innerHTML = htmlMascotas;
-    Array.from(document.getElementsByClassName("editar")).forEach((botonEditar)=>botonEditar.onclick=editar);
+    Array.from(document.getElementsByClassName("editar")).forEach((botonEditar, index)=>botonEditar.onclick=editar(index));
 }
 
 function enviarDatos(evento){
@@ -45,12 +45,42 @@ function enviarDatos(evento){
         raza : raza.value,
         dueno : dueno.value,
     };
-    mascotas.push(datos);
+    const accion = botonGuardar.innerHTML;
+    switch(accion){
+        case "Editar" :
+            mascotas[indice.value] = datos;
+            break;
+        default:
+            mascotas.push(datos);
+            break;    
+    }
+    
     listarMascotas();
+    resetModal();
 }
 
-function editar(evento) {
-    console.log("editar", evento);
+function editar(index) {
+    return function cuandoClick(){
+        botonGuardar.innerHTML = "Editar";  
+        $("#exampleModal").modal("toggle");
+        const mascota = mascotas[index];
+        especie.value = mascota.especie;
+        nombre.value = mascota.nombre;
+        edad.value = mascota.edad;
+        raza.value = mascota.raza;
+        dueno.value = mascota.dueno;
+        indice.value = index;
+    }
+}
+
+function resetModal(){
+    especie.value = "";
+    nombre.value = "";
+    edad.value = "";
+    raza.value = "";
+    dueno.value = "";
+    indice.value = "";
+    botonGuardar.innerHTML = "Crear";
 }
 
 listarMascotas();
