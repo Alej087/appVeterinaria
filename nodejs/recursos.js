be299d7a8a7554e9db97903d705d9bc70a08d215
Aviandro <aviandro@Lenoxs-MacBook-Pro.local>
2022-02-06 18:37:02 -0500

    recurso consultas con fechas automaticas

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -14,5 +14,9 @@ module.exports = {
     duenos: [
         {tipoDocumento : "C.C.", numeroDocumento : 1128268161, nombre : "Alejandro", apellido : "González", numeroContacto : 3012996400, direccion : "Cra 62 # 165 A 88 Torre 3 Apto 1203", email : "alejo87.gonzalez@gmail.com"},
         {tipoDocumento : "C.C.", numeroDocumento : 1019071711, nombre : "Lenox", apellido : "Almonacid", numeroContacto : 3017911456, direccion : "Cra 62 # 165 A 88 Torre 3 Apto 1203", email : "lenoxrico@gmail.com"},
-    ]
+    ],
+
+    consultas:[
+        {fechaCreacion: new Date(), fechaModificacion: new Date(), mascota: 0, veterinario: 0, historia: "", diagnostico: "", tratamiento: ""},
+    ],
 };
---
+++ nodejs/rutas/consultas.js
@@ -0,0 +1,46 @@
+module.exports = function consultasHandler (consultas) {
+    return {
+        get:  (data, callback) => {
+            if(typeof data.indice !== "undefined") {
+                if(consultas[data.indice]) {
+                    return callback(200, consultas[data.indice]);
+                }
+                return callback(404, {mensaje: `consulta con indice ${indice} no encontrada`});
+            }
+            callback(200, consultas);
+        },
+        post: (data, callback) => {
+            let nuevaConsulta = data.payload;
+            nuevaConsulta.fechaCreacion = new Date();
+            nuevaConsulta.fechaModificacion = null;
+            consultas = [...consultas, nuevaConsulta];
+            callback(201, nuevaConsulta);
+        },
+        put:  (data, callback) => {
+            if(typeof data.indice !== "undefined") {
+                if(consultas[data.indice]) {
+                    const {fechaCreacion} = consultas[data.indice];
+                    consultas[data.indice] = {
+                        ...data.payload, 
+                        fechaCreacion, 
+                        fechaModificacion: new Date(),
+                    };
+                    return callback(200, consultas[data.indice]);
+                }
+                return callback(404, {mensaje: `consulta con indice ${data.indice} no encontrada`});
+            }
+            callback(400, {mensaje: "indice no enviado"});
+        },
+        delete: (data, callback) => {
+            if(typeof data.indice !== "undefined") {
+                if(consultas[data.indice]) {
+                    consultas = consultas.filter((_consulta, indice) => indice != data.indice);
+                    return callback(204, {mensaje: `elemento con indice ${data.indice} eliminado`});
+                }
+                return callback(404, {mensaje: `consulta con indice ${data.indice} no encontrado`});
+            }
+            callback(400, {mensaje: "indice no enviado"});
+        },  
+    }
+}
+
