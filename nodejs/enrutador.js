a4f5ee706b81de677527ebfd684b7cfe3d84422a
Aviandro <aviandro@Lenoxs-MacBook-Pro.local>
2022-02-06 15:00:55 -0500

    Eliminar una mascota

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -22,11 +22,21 @@ module.exports =  {
                     global.recursos.mascotas[data.indice] = data.payload;
                     return callback(200, global.recursos.mascotas[data.indice]);
                 }
-                return callback(404, {mensaje: `mascota con indice ${indice} no encontrada`});
+                return callback(404, {mensaje: `mascota con indice ${data.indice} no encontrada`});
             }
             callback(400, {mensaje: "indice no enviado"});
         },
-    },    
+        delete: (data, callback) => {
+            if(typeof data.indice !== "undefined") {
+                if(global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas = global.recursos.mascotas.filter((_mascota, indice) => indice != data.indice);
+                    return callback(204, {mensaje: `elemento con indice ${data.indice} eliminado`});
+                }
+                return callback(404, {mensaje: `mascota con indice ${data.indice} no encontrada`});
+            }
+            callback(400, {mensaje: "indice no enviado"});
+        },  
+    },  
     noEncontrado: (data, callback) => {
         callback(404, {mensaje: "No encontrado"});
     },
--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -1,8 +1,8 @@
 module.exports = {
     mascotas: [
-        {especie: "Gato", nombre: "Mittens", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
-        {especie: "Gato", nombre: "Mittens", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
-        {especie: "Gato", nombre: "Mittens", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
-        {especie: "Gato", nombre: "Mittens", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
+        {especie: "Gato", nombre: "Mittens0", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
+        {especie: "Gato", nombre: "Mittens1", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
+        {especie: "Gato", nombre: "Mittens2", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
+        {especie: "Gato", nombre: "Mittens3", edad: "6 años", raza: "Criollo", dueno: "Alejandro González"},
     ],
 };
