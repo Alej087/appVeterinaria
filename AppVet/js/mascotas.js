6b055e9224687ce3e0142ec37cbfabf7f276cd5d
Aviandro <aviandro@Lenoxs-MacBook-Pro.local>
2022-02-08 21:27:42 -0500

    eliminar mascostas de la API desde el frontend y mensaje al quedar vacias

--- AppVet/js/mascotas.js
+++ AppVet/js/mascotas.js
@@ -15,26 +15,32 @@ async function listarMascotas() {
     try{
         const respuesta = await fetch(url);
         const mascotasDelServer = await respuesta.json();
-        if(Array.isArray(mascotasDelServer) && mascotasDelServer.length > 0) {
+        if(Array.isArray(mascotasDelServer)) {
             mascotas = mascotasDelServer;
         }
-        const htmlMascotas = mascotas.map((mascota, index)=> `<tr>
-            <th scope="row">${index}</th>
-            <td>${mascota.especie}</td>
-            <td>${mascota.nombre}</td>
-            <td>${mascota.edad}</td>
-            <td>${mascota.raza}</td>
-            <td>${mascota.dueno}</td>
-            <td>
-                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <button type="button" class="btn btn-info editar")"><i class="fas fa-edit"></i></button>
-                    <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash"></i></button>
-                </div>
-            </td>
-        </tr>`).join("");
-        listaMascotas.innerHTML = htmlMascotas;
-        Array.from(document.getElementsByClassName("editar")).forEach((botonEditar, index)=>botonEditar.onclick=editar(index));
-        Array.from(document.getElementsByClassName("eliminar")).forEach((botonEliminar, index)=>botonEliminar.onclick=eliminarMascota(index));
+        if(mascotas.length > 0) {
+            const htmlMascotas = mascotas.map((mascota, index)=> `<tr>
+                <th scope="row">${index}</th>
+                <td>${mascota.especie}</td>
+                <td>${mascota.nombre}</td>
+                <td>${mascota.edad}</td>
+                <td>${mascota.raza}</td>
+                <td>${mascota.dueno}</td>
+                <td>
+                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                        <button type="button" class="btn btn-info editar")"><i class="fas fa-edit"></i></button>
+                        <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash"></i></button>
+                    </div>
+                </td>
+            </tr>`).join("");
+            listaMascotas.innerHTML = htmlMascotas;
+            Array.from(document.getElementsByClassName("editar")).forEach((botonEditar, index)=>botonEditar.onclick=editar(index));
+            Array.from(document.getElementsByClassName("eliminar")).forEach((botonEliminar, index)=>botonEliminar.onclick=eliminarMascota(index));
+            return;
+        }
+        listaMascotas.innerHTML = `<tr>
+            <td colspan="5">No hay mascotas</td>
+        </tr>`;
     } catch (error){
         throw error;
     }
@@ -101,9 +107,18 @@ function resetModal(){
 }
 
 function eliminarMascota(index){
-    return function cuandoClickEliminar(){
-        mascotas = mascotas.filter((mascota,indiceMascota)=>indiceMascota !== index);
-        listarMascotas();
+    const urlEnvio = `${url}/${index}`;
+    return async function cuandoClickEliminar(){
+        try {
+            const respuesta = await fetch(urlEnvio, {
+                method: "DELETE",
+            }); 
+            if (respuesta.ok) {
+                listarMascotas();
+            }       
+        } catch (error) {
+            throw error;
+        }       
     };
 }
 
