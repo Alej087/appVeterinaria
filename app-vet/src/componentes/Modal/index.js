1387e2263a8611dc933b753556d1674236972fa5
Aviandro <aviandro@Lenoxs-MacBook-Pro.local>
2022-03-16 15:35:08 -0500

    estilos css a la modal

--- app-vet/src/componentes/Modal/index.js
+++ app-vet/src/componentes/Modal/index.js
@@ -1,39 +1,43 @@
 import React from "react";
 import ModalHeader from "./modalHeader";
 import Select from "../Select";
+import "./modal.css"
 
 function Modal () {
     return (
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <ModalHeader />
-                    <div className="modal-body">
-                        <form id="form">
-                            <div className="form-row">
-                                <Select />
-                                <div className="col">
-                                    <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Nombre"/>
+        <>
+            <div className="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <ModalHeader />
+                        <div className="modal-body">
+                            <form id="form">
+                                <div className="form-row">
+                                    <Select />
+                                    <div className="col">
+                                        <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Nombre"/>
+                                    </div>
+                                    <div className="col">
+                                        <input type="text" id="raza" name="raza" className="form-control" placeholder="Raza"/>
+                                    </div>
+                                    <div className="col">
+                                        <input type="text" id="edad" name="edad" className="form-control" placeholder="Edad"/>
+                                    </div>
+                                    <div className="col">
+                                        <input type="text" id="dueno" name="dueno" className="form-control" placeholder="Dueño"/>
+                                    </div>
                                 </div>
-                                <div className="col">
-                                    <input type="text" id="raza" name="raza" className="form-control" placeholder="Raza"/>
-                                </div>
-                                <div className="col">
-                                    <input type="text" id="edad" name="edad" className="form-control" placeholder="Edad"/>
-                                </div>
-                                <div className="col">
-                                    <input type="text" id="dueno" name="dueno" className="form-control" placeholder="Dueño"/>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="button" id="btn-guardar" className="btn btn-primary" data-bs-dismiss="modal">Crear</button>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="button" id="btn-guardar" className="btn btn-primary" data-bs-dismiss="modal">Crear</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class = "modal-backdrop fade show"></div>
+        </>
     )
 }
 
