ee7ade22bfafdbaab9599b95f18477b69985f0f4
Alejandro González <alejo@iMac-de-Alejandro.local>
2022-04-07 23:04:10 -0500

    Se crea componente Input

--- app-vet/src/componentes/Modal/index.js
+++ app-vet/src/componentes/Modal/index.js
@@ -1,48 +1,80 @@
 import React from "react";
 import ModalHeader from "./modalHeader";
+import Input from "../Input";
 import Select from "../Select";
-import "./modal.css"
+import "./modal.css";
 
-function Modal () {
+function Modal() {
     return (
         <>
-            <div className="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div
+                className="modal"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <ModalHeader />
                         <div className="modal-body">
                             <form id="form">
                                 <div className="form-row">
-                                    <Select options = {[
-                                                        {valor: "Perro", etiqueta: "Perro"},
-                                                        {valor: "Gato", etiqueta: "Gato"},
-                                                        {valor: "Otro", etiqueta: "Otro"},
-                                                        ]} nombreCampo = "Especie"/>
+                                    <Select
+                                        options={[
+                                            {
+                                                valor: "Perro",
+                                                etiqueta: "Perro",
+                                            },
+                                            { valor: "Gato", etiqueta: "Gato" },
+                                            { valor: "Otro", etiqueta: "Otro" },
+                                        ]}
+                                        nombreCampo="Especie"
+                                    />
                                     <div className="col">
-                                        <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Nombre"/>
+                                        <Input
+                                            tipo="text"
+                                            nombreCampo="Nombre"
+                                        />
                                     </div>
                                     <div className="col">
-                                        <input type="text" id="raza" name="raza" className="form-control" placeholder="Raza"/>
+                                        <Input tipo="text" nombreCampo="Raza" />
                                     </div>
                                     <div className="col">
-                                        <input type="text" id="edad" name="edad" className="form-control" placeholder="Edad"/>
+                                        <Input tipo="text" nombreCampo="Edad" />
                                     </div>
                                     <div className="col">
-                                        <input type="text" id="dueno" name="dueno" className="form-control" placeholder="Dueño"/>
+                                        <Input
+                                            tipo="text"
+                                            nombreCampo="Dueño"
+                                        />
                                     </div>
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                            <button type="button" id="btn-guardar" className="btn btn-primary" data-bs-dismiss="modal">Crear</button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Cerrar
+                            </button>
+                            <button
+                                type="button"
+                                id="btn-guardar"
+                                className="btn btn-primary"
+                                data-bs-dismiss="modal"
+                            >
+                                Crear
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class = "modal-backdrop fade show"></div>
+            <div class="modal-backdrop fade show"></div>
         </>
-    )
+    );
 }
 
 export default Modal;
